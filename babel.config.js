622c1285f081b887ffb5d43163102d2d6bbeee50
Rodrigo Fernandes <digonalha@gmail.com>
2020-04-18 01:10:30 -0300

    init repo

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // import export, arrow function...
+    "@babel/preset-react" // jsx e funcionalidades do react
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom' // responsavel por renderizar um componente react no nosso html
+import App from './App';
+
+render(<App />, document.getElementById('app'));
