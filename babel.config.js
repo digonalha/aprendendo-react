41cda0947ff9d3995c37b6c1352bb6e89ce52c42
Rodrigo Fernandes <digonalha@gmail.com>
2020-04-23 10:28:45 -0300

    melhorias nos class components

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ module.exports = {
   presets: [
     "@babel/preset-env", // import export, arrow function...
     "@babel/preset-react" // jsx e funcionalidades do react
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
   ]
 }
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import TechItem from './TechItem';
 
 class TechList extends Component {
-  render() {
-    state = { techs: [], newTech: ''}
+  state = {
+    techs: [
+      'NodeJS',
+      'ReactJS',
+      'ReactNative',
+    ],
+    newTech: ''
+  };
 
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech)
+    })
+  }
+
+  render() {
+    console.log(this.state);
     return (
-      <>
-      <ul>
-        <li>Nodejs</li>
-        <li>Reactjs</li>
-        <li>ReactNative</li>
-      </ul>
-      </>
+      <form onSubmit={this.handleSubmit}>
+        <h1>Tecnologias:</h1>
+        <ul>
+          {this.state.techs.map(tech =>
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          )}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech} />
+        <button type="">Enviar</button>
+      </form>
     )
   }
 }
