6e0e7fc3b99f18757047f69b90f0441a236cd812
Rodrigo Fernandes <digonalha@gmail.com>
2020-04-23 23:42:58 -0300

    finalizando conceito de classes

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,13 +3,30 @@ import TechItem from './TechItem';
 
 class TechList extends Component {
   state = {
-    techs: [
-      'NodeJS',
-      'ReactJS',
-      'ReactNative',
-    ],
+    techs: [],
     newTech: ''
   };
+
+  //executa assim que o componente aparece em tela
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+
+    if(techs) {
+      this.setState( { techs: JSON.parse(techs)})
+    }
+  }
+  // executado sempre que houver alteracoes nas props ou estado
+  componentDidUpdate(_, prevState) {
+    //this.props OU this.state 
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // executado quando o documento deixa de existir
+  componentWillMount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -33,7 +50,7 @@ class TechList extends Component {
     console.log(this.state);
     return (
       <form onSubmit={this.handleSubmit}>
-        <h1>Tecnologias:</h1>
+        <h1>Tecnologias</h1>
         <ul>
           {this.state.techs.map(tech =>
             <TechItem
